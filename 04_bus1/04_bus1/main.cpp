e1424e820364176be79bc9782b16d309fa406dc6
Sergey <enzzomatrix@gmail.com>
2020-07-22 22:05:18 +1100

    upd

---
+++ 04_bus1/04_bus1/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+int main(int args, char* argv) {
+
+	int n;
+	std::cin >> n;
+	std::string mode;
+
+	std::map<std::string, std::vector<std::string>> bus_line;
+
+	while (n-- > 0) {
+		std::cin >> mode;
+
+		if (mode == "NEW_BUS") {
+			std::string bus;
+			int count;
+			std::cin >> bus >> count;
+			std::vector<std::string>names(count);
+
+			for (std::string& s : names) std::cin >> s;
+			bus_line[bus] = names;
+
+		}
+		else if (mode == "BUSES_FOR_STOP") {
+			//OUTPUT
+				//NO STOP
+				//[bus_1] [bus_n]
+		}
+		else if (mode == "STOPS_FOR_BUS") {
+			//OUTPUT:
+				//NO BUS
+				//ÑÏÈÑÎÊ
+					//Stop [stop]: [bus_1] [bus_n] ...
+					//Stop [stop]: no interchange
+		}
+		else if (mode == "ALL_BUSES") {
+			if (bus_line.size() > 0) {
+				for (const std::pair<std::string, std::vector<std::string>>& p : bus_line) {
+					std::cout << "Bus " << p.first << ": ";
+					for (const std::string& s : p.second) {
+						std::cout << s << " ";
+					}					
+				}
+			}
+			else {
+				std::cout << "No buses";
+			}
+			std::cout << std::endl;
+		}
+		//WRONG MODE:
+		else {
+			n++;
+		}
+	}
+
+	return 0;
+}
