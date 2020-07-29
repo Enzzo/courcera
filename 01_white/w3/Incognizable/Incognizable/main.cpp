8c5c84fef34e54f20458025cc32b9c2a05d8d1c8
Sergey Vasilev <enzzomatrix@gmail.com>
2020-07-29 17:13:41 +1100

    upd

---
+++ 01_white/w3/Incognizable/Incognizable/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Incognizable {
+public:
+	Incognizable() :x(0),y(0){};
+	Incognizable(int a) :x(a),y(0) {};
+	Incognizable(int a, int b) :x(a), y(b) {};
+private:
+	int x, y;
+};
+
+int main() {
+	
+	Incognizable a;
+	Incognizable b = {};
+	Incognizable c = { 0 };
+	Incognizable d = { 0, 1 };
+
+	return 0;
+}
+
+
