40becc2f58f4167439089a9bf020336a1cd5a377
Sergey <enzzomatrix@gmail.com>
2020-09-08 14:43:57 +1100

    upd

--- 02/w4/[07]mergesort/[07]mergesort/main.cpp
+++ 02/w4/[07]mergesort/[07]mergesort/main.cpp
@@ -39,17 +39,45 @@ void MergeSort(RandomIt begin, RandomIt end) {
 
 }
 
+//УДАЛИТЬ ДВОЙКУ В НАЗВАНИИ ФУНКЦИИ И ВНУТРИ ФУНКЦИИ!!!
 template<typename RandomIt>
-void Test(RandomIt i) {
-	std::advance(i);
-}
-int main() {
-	std::vector<int> v{ 6, 4, 7, 6, 4, 4, 0, 1 };
-	
-	MergeSort(v.begin(), v.end());
+void MergeSort2(RandomIt begin, RandomIt end) {
+	if (begin == end || begin + 1 == end || begin + 2 == end)return;
 
-	for (const int i : v)
-		std::cout << i << " ";
+	//Создать вектор, содержащий все элементы текущего диапазона
+	std::vector<RandomIt::value_type> elements(begin, end);
+
+	//Разбить вектор на три равные части
+	size_t step = elements.size() / 3;
+	RandomIt mid1 = begin + step;
+	RandomIt mid2 = mid1  + step;
+
+	//Вызвать функцию MergeSort от каждой половины вектора
+	MergeSort2(begin, mid1);
+	MergeSort2(mid1, mid2);
+	MergeSort2(mid2, end);
+
+	elements.clear();
+
+	//???
+	std::sort(begin, end);
+	//std::sort(mid, end);
+
+	//С помощью алгоритма merge слить отсортированные половины, записав полученный отсортированный диапазон вместо исходного
+	std::merge(begin, mid1, mid1, mid2, std::back_inserter(elements));
+	
+	elements.clear();
+	std::merge(mid1, mid2, mid2, end, std::back_inserter(elements));
+
+}
+
+int main() {
+	std::vector<int> v = { 6, 4, 7, 6, 4, 4, 0, 1, 5 };
+	MergeSort2(begin(v), end(v));
+	for (int x : v) {
+		std::cout << x << " ";
+	}
+	std::cout << std::endl;
 
 	return 0;
 }
