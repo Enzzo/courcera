00ffa33788784627e034b566bd3fc8e62aa9448f
Sergey <enzzomatrix@gmail.com>
2020-09-10 17:35:34 +1100

    upd

--- 02/w4/[10]/T1_find_with_pref/T1_find_with_pref/main.cpp
+++ 02/w4/[10]/T1_find_with_pref/T1_find_with_pref/main.cpp
@@ -10,7 +10,7 @@ std::pair<RandomIt, RandomIt> FindStartsWith(RandomIt, RandomIt, char);
 int main() {
     const std::vector<std::string> sorted_strings = { "moscow", "murmansk", "vologda" };
 
-    const auto m_result = FindStartsWith(sorted_strings.begin(), sorted_strings.end(), 'm');
+    const auto m_result = FindStartsWith(sorted_strings.begin(), sorted_strings.end(), 'v');
     for (auto it = m_result.first; it != m_result.second; ++it) {
         std::cout << *it << " "; 
     }                                                                                       
@@ -31,12 +31,20 @@ int main() {
     return 0;
 }
 
+bool pred(std::string& lhs, const std::string& rhs) {
+    return lhs[0] < rhs[0];
+}
+
 template<typename RandomIt>
 std::pair<RandomIt, RandomIt>FindStartsWith(RandomIt range_begin, RandomIt range_end, char prefix) {
     RandomIt i1 = range_begin;
-    RandomIt i2;
+    RandomIt i2 = range_begin;
 
-
+    //хыел LOWER_BOUND
+    i1 = std::lower_bound(range_begin, range_end, prefix, &pred);
+    //ябепъел, врнаш мскебни щкелемр хрепюрнпю ашк пюбем опетхйяс
+    //хыел брнпни LOWER_BOUND
+    //ябепъел, врнаш мскебни щкелемр брнпнцн хрепюрнпю ашк пюбем опетхйяс
 
     return { i1, i2 };
 };
