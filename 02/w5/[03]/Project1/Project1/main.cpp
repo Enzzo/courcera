c8bb68c805ab11498c7d8d71f764b25edeaf5dca
Sergey <enzzomatrix@gmail.com>
2020-09-18 15:30:25 +1100

    upd

--- 02/w5/[03]/Project1/Project1/main.cpp
+++ 02/w5/[03]/Project1/Project1/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <sstream>
 #include <iomanip>
+#include <fstream>
 
 class Figure {
 public:
@@ -13,7 +14,10 @@ public:
 
 class Rect : public Figure {
 public:
-    Rect(const double s1, const double s2) : perimeter((s1 + s2)*2) , area(s1 * s2) {};
+    Rect(const double s1, const double s2){
+        perimeter = (s1 + s2) * 2;
+        area = s1 * s2;
+    };
     virtual std::string Name()const override {
         return "RECT";
     }
@@ -24,13 +28,17 @@ public:
         return area;
     }
 private:
-    const double perimeter;
-    const double area;
+    double perimeter;
+    double area;
 };
 
 class Triangle : public Figure {
 public:
-    Triangle(const double s1, const double s2, const double s3) : perimeter(s1 + s2 + s3), ;
+    Triangle(const double s1, const double s2, const double s3){
+        perimeter = s1 + s2 + s3;
+        double p = perimeter / 2;
+        area = std::sqrt(p * (p - s1) * (p - s2) * (p - s3));
+    }
     virtual std::string Name()const override {
         return "TRIANGLE";
     }
@@ -41,19 +49,65 @@ public:
         return area;
     }
 private:
-    const double perimeter;
-    const double area;
+    double perimeter;
+    double area;
+};
+
+class Circle : public Figure {
+public:
+    Circle(const double r){
+        const double pi = 3.14;
+        perimeter = 2 * pi * r;
+        area = pi * r * r;
+    }
+    virtual std::string Name()const override {
+        return "CIRCLE";
+    }
+    virtual double Perimeter()const override {
+        return perimeter;
+    }
+    virtual double Area()const override {
+        return area;
+    }
+private:
+    double perimeter;
+    double area;
 };
 std::shared_ptr<Figure> CreateFigure(std::istream& is) {
     
-    std::shared_ptr<Figure> f = std::make_shared<Rect> (2, 3);
+    std::shared_ptr<Figure> f;
+    
+    std::string type;
+    is >> type;
+    is >> std::ws;
+    
+    std::vector<double> sides;
+    std::string s;
+    double x;
 
+    while (is >> x) {
+        sides.push_back(x);
+    }
+
+    if (type == "TRIANGLE") {
+        f = std::make_shared<Triangle>(sides[0], sides[1], sides[2]);
+    }
+    else if (type == "RECT") {
+        f = std::make_shared<Rect>(sides[0], sides[1]);
+    }
+    else if (type == "CIRCLE") {
+        f = std::make_shared<Circle>(sides[0]);
+    }
+    
     return f;
 }
 
 int main() {
+    ////////////////////////////////////////////////////////////
+    std::ifstream ifs("input.txt");
+    ////////////////////////////////////////////////////////////
     std::vector<std::shared_ptr<Figure>> figures;
-    for (std::string line; std::getline(std::cin, line); ) {
+    for (std::string line; std::getline(ifs, line); ) {
         std::istringstream is(line);
 
         std::string command;
